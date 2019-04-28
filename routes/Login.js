bdab2e578f697077647571a8b1911744dab3cd99
Gerardo11011 <gerardo_apg_97@hotmail.com>
2019-04-28 15:19:46 -0500

    Cambios en los archivos pug

--- routes/Login.js
+++ routes/Login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('login')
+  res.render('login', {title: 'login'})
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('index', {title : 'Derivando el cielo', logeado: false})
+  res.render('index', {title : 'Derivando el cielo', logeado: false, title: 'Index'})
 });
 
 module.exports = router;
