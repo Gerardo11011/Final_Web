3f0425434aedfa6d2673fab92913a473f0612bdb
Gerardo11011 <gerardo_apg_97@hotmail.com>
2019-04-29 22:51:01 -0500

    cambios con mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/webDB', {useNewUrlParser: true});
+var db = mongoose.connection;
+var art = require ('../models/article')
 
 router.get('/', function (req, res, next) {
-  res.render('index', {title : 'Derivando el cielo', logeado: false, title: 'Index'})
+  art.find({}, function (err, art){
+
+    if(err){
+       console.log(err);
+    }
+    else {
+       console.log(art);
+       res.render('Fisica', {title : 'Fisica', logeado: false, article: art});
+    }
+  });
 });
 
 module.exports = router;
